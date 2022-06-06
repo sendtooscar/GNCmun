2ddd756f121e1fd9d170544e3e27dd4cccc92564
Oscar De Silva <sendtooscar@gmail.com>
2022-06-06 02:38:42 -0230

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import numpy as np
 import math 
 
-print("The following functions are loaded :  rotnp('axis',angle)")
+print("The following functions are loaded :  rotnp('axis',angle)  skew(v)  wrapToPi(angle)")
 
 def rotnp(a,b):   
       c=math.cos(b)
@@ -20,3 +20,31 @@ def rotnp(a,b):
         [0,1,0],
         [-s,0,c]])
       return(R)
+
+def skew(v):
+    if len(v.shape) is not 1:
+      raise ValueError("v should be 1 dimentional")
+    elif v.shape[0] is not 3:
+      raise ValueError("v should be 1 dimentional and have 3 elemnets")
+    else:
+      V = np.array( [[ 0, -v[2], v[1]],
+                    [v[2],  0,  -v[0]],
+                    [-v[1],  v[0],   0]])
+    return V
+
+
+def wrapToPi(x):
+    xwrap = np.remainder(x, 2 * np.pi)
+    mask = np.abs(xwrap) > np.pi
+    if len(x.shape) is not 1 :
+      if mask :
+        xwrap -= 2 * np.pi * np.sign(xwrap)
+      else :
+        xwrap = x
+    else :
+      xwrap[mask] -= 2 * np.pi * np.sign(xwrap[mask])
+    #mask1 = x < 0
+    #mask2 = np.remainder(x, np.pi) == 0
+    #mask3 = np.remainder(x, 2 * np.pi) != 0
+    #xwrap[mask1 & mask2 & mask3] -= 2 * np.pi
+    return xwrap
