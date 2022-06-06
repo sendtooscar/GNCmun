e7e8430ac25acddfdac2ffdeb36eff7436271ced
Oscar De Silva <sendtooscar@gmail.com>
2022-06-06 16:16:35 -0230

    Update gnc_animations.py

--- gnc_animations.py
+++ gnc_animations.py
@@ -33,3 +33,43 @@ def animate_isl_pendulum(X,U,T,param,fps,duration):
 
   plt.close()
   return ani
+
+
+def animate_2Dbot(X,U,T,param,robot_scale,frame_num,speedup):
+  robot_scale =0.5;
+  fig = plt.figure()
+  ax = fig.add_subplot(aspect='equal')
+  ax.set_xlim(-2.2, 2.2)
+  ax.set_ylim(-2.2, 2.2)
+  triangle = ax.add_patch(plt.Polygon(np.array([[-0.4,0.3],[-0.4,-0.3],[0.4,-0.01],[1.2,0],[0.4,0.01]])*robot_scale, True))
+  line, = ax.plot([0, 0,3], [0, -1,5], lw=1, c='c', alpha=0.5) # draw a line 
+  line.set_data([0],[0])
+  transform = mpl.transforms.Affine2D().rotate_deg(-45) + mpl.transforms.Affine2D().translate(-5,-5) 
+  triangle.set_transform(transform+ax.transData)
+
+  # determine skip amount from number of frames
+  #frame_num =300
+  #speedup =2
+  tail = 15
+  skip = np.floor(len(X)/frame_num)
+  if skip < 1:
+    skip=1
+    frame_num = len(X)
+  skip=int(skip)
+
+  def animate(i):
+    #print(i)
+    transform = mpl.transforms.Affine2D().rotate(X[skip*i][2]) + mpl.transforms.Affine2D().translate(X[skip*i][0],X[skip*i][1]) 
+    triangle.set_transform(transform+ax.transData)
+    if i>1:
+      if i>tail:
+        Xtemp= np.array(X[i-tail:i])
+        line.set_data(Xtemp[:,0],Xtemp[:,1])
+      else:
+        Xtemp= np.array(X[0:i])
+        line.set_data(Xtemp[:,0],Xtemp[:,1])
+
+  ani = animation.FuncAnimation(fig, animate, frames=frame_num, repeat=True, interval=int(dt*1000*skip/speedup)) # interval determines speed in ms
+  print("Number of frames :",frame_num)
+  plt.close()
+  return ani
